03f14a9264107d8a30a2232a53c494bf94513dc0
spirozh <jason.songhurst@gmail.com>
2023-06-21 23:48:47 +0300

    wranging routes and tempates...

--- html/html.go
+++ html/html.go
@@ -7,33 +7,23 @@ import (
 	"log"
 )
 
-// go:embed  templates/*
+//go:embed  templates/*
 var templates embed.FS
 
-// go:embed  static/*
-var static embed.FS
+//go:embed  static/*
+var Static embed.FS
 
 // pages
 var (
 	index = parse("index.html")
 )
 
-func Static(w io.Writer, path string) error {
-	f, err := static.Open(path)
-	if err != nil {
-		return err
-	}
-
-	_, err = io.Copy(w, f)
-	return err
-}
-
 func Index(w io.Writer) error {
 	return index.Execute(w, nil)
 }
 
 func parse(file string) *template.Template {
-	x, err := template.New("layout.html").ParseFS(templates, "layout.html", file)
+	x, err := template.New(file).ParseFS(templates, "templates/layout.html", "templates/"+file)
 	if err != nil {
 		log.Fatalf("parsing file %s: %r", file, err)
 	}
--- http/routes.go
+++ http/routes.go
@@ -5,6 +5,8 @@ import (
 	"io"
 	"net/http"
 	"time"
+
+	"github.com/spirozh/timr/html"
 )
 
 func Selma(w http.ResponseWriter, r *http.Request) {
@@ -35,10 +37,72 @@ func Long(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func TemplateRoutes() http.Handler {
+	m := http.NewServeMux()
+	return m
+}
+
+func staticHandler(w http.ResponseWriter, r *http.Request) {
+	// assume GET
+	io.WriteString(w, "static handler\n")
+
+}
+
+func APIRoutes() http.Handler {
+	m := http.NewServeMux()
+
+	return m
+}
+
+func SSERoutes() http.Handler {
+	m := http.NewServeMux()
+
+	m.HandleFunc("/api/sse/long/", Long)
+
+	return m
+}
+
+func static() http.HandlerFunc {
+	// http.FileServer(http.FS(html.Static))
+
+	efs := html.Static
+	hfs := http.FS(efs)
+
+	fs := http.FileServer(hfs)
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		files := []string{"/static/hello.txt", "static/hello.txt", "/hello.txt", "hello.txt"}
+		for _, file := range files {
+			_, e := efs.Open(file)
+			if e != nil {
+				fmt.Println("error!", e)
+				continue
+			}
+			fmt.Println("ok: ", file)
+		}
+
+		fmt.Println(r.URL.Path)
+		fs.ServeHTTP(w, r)
+	}
+
+}
+
 func routes() http.Handler {
 	m := http.NewServeMux()
 
-	m.HandleFunc("/long/", Long)
+	// templates
+	m.Handle("/", TemplateRoutes())
+
+	// static
+	//	m.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(html.Static))))
+	m.Handle("/static/", static())
+
+	// api
+	m.Handle("/api/", APIRoutes())
+
+	// sse
+	m.Handle("/api/sse/", SSERoutes())
+
 	m.HandleFunc("/selma", Selma)
 
 	return m
