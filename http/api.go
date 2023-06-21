d666c855814321b354a10b75e93659112ab8cba7
spirozh <jason.songhurst@gmail.com>
2023-06-21 23:57:10 +0300

    move handlers to api.go \

--- http/api.go
+++ http/api.go
@@ -1,1 +1,38 @@
 package http
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+// Selma handles /selma
+func Selma(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "hello selma!!\n")
+}
+
+// Long handles /sse/long/
+func Long(w http.ResponseWriter, r *http.Request) {
+	// Make sure that the writer supports flushing.
+	//
+	flusher, ok := w.(http.Flusher)
+
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	for i := 0; ; i++ {
+
+		fmt.Fprintf(w, "data: %d\n\n", i)
+		flusher.Flush()
+
+		time.Sleep(time.Second)
+	}
+}
--- http/routes.go
+++ http/routes.go
@@ -1,51 +1,14 @@
 package http
 
 import (
-	"fmt"
-	"io"
 	"net/http"
-	"time"
 
 	"github.com/spirozh/timr/html"
 )
 
-func Selma(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "hello selma!!\n")
-}
-
-func Long(w http.ResponseWriter, r *http.Request) {
-	// Make sure that the writer supports flushing.
-	//
-	flusher, ok := w.(http.Flusher)
-
-	if !ok {
-		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "text/event-stream")
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	for i := 0; ; i++ {
-
-		fmt.Fprintf(w, "data: %d\n\n", i)
-		flusher.Flush()
-
-		time.Sleep(time.Second)
-	}
-}
-
 func TemplateRoutes() http.Handler {
 	m := http.NewServeMux()
 	return m
-}
-
-func staticHandler(w http.ResponseWriter, r *http.Request) {
-	// assume GET
-	io.WriteString(w, "static handler\n")
-
 }
 
 func APIRoutes() http.Handler {
@@ -62,31 +25,6 @@ func SSERoutes() http.Handler {
 	return m
 }
 
-func static() http.HandlerFunc {
-	// http.FileServer(http.FS(html.Static))
-
-	efs := html.Static
-	hfs := http.FS(efs)
-
-	fs := http.FileServer(hfs)
-
-	return func(w http.ResponseWriter, r *http.Request) {
-		files := []string{"/static/hello.txt", "static/hello.txt", "/hello.txt", "hello.txt"}
-		for _, file := range files {
-			_, e := efs.Open(file)
-			if e != nil {
-				fmt.Println("error!", e)
-				continue
-			}
-			fmt.Println("ok: ", file)
-		}
-
-		fmt.Println(r.URL.Path)
-		fs.ServeHTTP(w, r)
-	}
-
-}
-
 func routes() http.Handler {
 	m := http.NewServeMux()
 
@@ -94,8 +32,7 @@ func routes() http.Handler {
 	m.Handle("/", TemplateRoutes())
 
 	// static
-	//	m.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(html.Static))))
-	m.Handle("/static/", static())
+	m.Handle("/static/", http.FileServer(http.FS(html.Static)))
 
 	// api
 	m.Handle("/api/", APIRoutes())
@@ -103,7 +40,9 @@ func routes() http.Handler {
 	// sse
 	m.Handle("/api/sse/", SSERoutes())
 
+	// selma
 	m.HandleFunc("/selma", Selma)
+	m.HandleFunc("/selma/", Selma)
 
 	return m
 }
