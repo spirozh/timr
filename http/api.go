118ec7cf0f97bf299a6c679bf187f46cfad881b8
spirozh <jason.songhurst@gmail.com>
2023-06-26 10:14:52 +0300

    move stuff around

--- http/api.go
+++ http/api.go
@@ -3,16 +3,27 @@ package http
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
+	"strconv"
+	"strings"
 	"sync"
+	"time"
 
 	"github.com/spirozh/timr"
 )
 
-// Selma handles /selma
-func Selma(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "hello selma!!\n")
+func apiRoutes(ts timr.TimerService) http.Handler {
+	m := http.NewServeMux()
+
+	m.HandleFunc("/api/create/", createTimer(ts)) // /api/create/name/duration
+	m.HandleFunc("/api/pause/", pauseTimer(ts))   // /api/pause/name
+	m.HandleFunc("/api/resume/", resumeTimer(ts)) // /api/resume/name
+	m.HandleFunc("/api/delete/", deleteTimer(ts)) // /api/delete/name
+
+	// sse route
+	m.HandleFunc("/api/sse", SSE(ts))
+
+	return m
 }
 
 func sseSetup(w http.ResponseWriter, r *http.Request) (flusher http.Flusher, ok bool) {
@@ -89,5 +100,67 @@ func SSE(ts timr.TimerService) http.HandlerFunc {
 		<-r.Context().Done()
 
 		fmt.Print("EXITING SSE")
+	}
+}
+
+func createTimer(ts timr.TimerService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		arg, ok := strings.CutPrefix(r.URL.Path, "/api/create/")
+		args := strings.Split(arg, "/")
+		if !ok || len(args) != 2 {
+			fmt.Fprintf(w, "bad request: %s/n", r.URL.Path)
+		}
+
+		t, err := strconv.Atoi(args[1])
+		if err == nil {
+			err = ts.Create(args[0], time.Duration(t)*time.Millisecond)
+		}
+
+		fmt.Fprintf(w, "create: %v, err: %v\n", r.URL, err)
+	}
+}
+
+func pauseTimer(ts timr.TimerService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name, ok := strings.CutPrefix(r.URL.Path, "/api/pause/")
+		if !ok {
+			fmt.Fprintf(w, "bad request: %s/n", r.URL.Path)
+		}
+
+		var err error
+		if t, err := ts.Get(name); err == nil {
+			t.Pause()
+		}
+
+		fmt.Fprintf(w, "pause: %v, err: %v\n", r.URL, err)
+	}
+}
+
+func resumeTimer(ts timr.TimerService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name, ok := strings.CutPrefix(r.URL.Path, "/api/resume/")
+		if !ok {
+			fmt.Fprintf(w, "bad request: %s/n", r.URL.Path)
+		}
+
+		var err error
+		if t, err := ts.Get(name); err == nil {
+			t.Resume()
+		}
+
+		fmt.Fprintf(w, "resume: %v, err: %v\n", r.URL, err)
+	}
+}
+
+func deleteTimer(ts timr.TimerService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name, ok := strings.CutPrefix(r.URL.Path, "/api/delete/")
+		if !ok {
+			fmt.Fprintf(w, "bad request: %s/n", r.URL.Path)
+		}
+
+		err := ts.Remove(name)
+
+		fmt.Fprintf(w, "delete: %v, err: %v\n", r.URL, err)
 	}
 }
--- http/routes.go
+++ http/routes.go
@@ -1,102 +1,12 @@
 package http
 
 import (
-	"fmt"
+	"io"
 	"net/http"
-	"strconv"
-	"strings"
-	"time"
 
 	"github.com/spirozh/timr"
 	"github.com/spirozh/timr/html"
 )
-
-func TemplateRoutes() http.Handler {
-	m := http.NewServeMux()
-	return m
-}
-
-func nothingYet(w http.ResponseWriter, r *http.Request) {
-	msg := fmt.Sprintf("Path: %s (not handled)\n", r.URL.Path)
-	fmt.Print(msg)
-	fmt.Fprint(w, msg)
-}
-
-func createTimer(ts timr.TimerService) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		arg, ok := strings.CutPrefix(r.URL.Path, "/api/create/")
-		args := strings.Split(arg, "/")
-		if !ok || len(args) != 2 {
-			fmt.Fprintf(w, "bad request: %s/n", r.URL.Path)
-		}
-
-		t, err := strconv.Atoi(args[1])
-		if err == nil {
-			err = ts.Create(args[0], time.Duration(t)*time.Millisecond)
-		}
-
-		fmt.Fprintf(w, "create: %v, err: %v\n", r.URL, err)
-	}
-}
-
-func pauseTimer(ts timr.TimerService) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		name, ok := strings.CutPrefix(r.URL.Path, "/api/pause/")
-		if !ok {
-			fmt.Fprintf(w, "bad request: %s/n", r.URL.Path)
-		}
-
-		var err error
-		if t, err := ts.Get(name); err == nil {
-			t.Pause()
-		}
-
-		fmt.Fprintf(w, "pause: %v, err: %v\n", r.URL, err)
-	}
-}
-
-func resumeTimer(ts timr.TimerService) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		name, ok := strings.CutPrefix(r.URL.Path, "/api/resume/")
-		if !ok {
-			fmt.Fprintf(w, "bad request: %s/n", r.URL.Path)
-		}
-
-		var err error
-		if t, err := ts.Get(name); err == nil {
-			t.Resume()
-		}
-
-		fmt.Fprintf(w, "resume: %v, err: %v\n", r.URL, err)
-	}
-}
-
-func deleteTimer(ts timr.TimerService) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		name, ok := strings.CutPrefix(r.URL.Path, "/api/delete/")
-		if !ok {
-			fmt.Fprintf(w, "bad request: %s/n", r.URL.Path)
-		}
-
-		err := ts.Remove(name)
-
-		fmt.Fprintf(w, "delete: %v, err: %v\n", r.URL, err)
-	}
-}
-
-func APIRoutes(ts timr.TimerService) http.Handler {
-	m := http.NewServeMux()
-
-	m.HandleFunc("/api/create/", createTimer(ts)) // /api/create/name/duration
-	m.HandleFunc("/api/pause/", pauseTimer(ts))   // /api/pause/name
-	m.HandleFunc("/api/resume/", resumeTimer(ts)) // /api/resume/name
-	m.HandleFunc("/api/delete/", deleteTimer(ts)) // /api/delete/name
-
-	// sse route
-	m.HandleFunc("/api/sse", SSE(ts))
-
-	return m
-}
 
 func routes(ts timr.TimerService) http.Handler {
 	m := http.NewServeMux()
@@ -105,11 +15,16 @@ func routes(ts timr.TimerService) http.Handler {
 	m.Handle("/", http.FileServer(http.FS(html.FS)))
 
 	// api
-	m.Handle("/api/", APIRoutes(ts))
+	m.Handle("/api/", apiRoutes(ts))
 
 	// selma
 	m.HandleFunc("/selma", Selma)
 	m.HandleFunc("/selma/", Selma)
 
 	return m
+}
+
+// Selma handles /selma
+func Selma(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello Selma!!\n")
 }
