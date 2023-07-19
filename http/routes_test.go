5c892e926c8584d0e64b69f25ed06bb66fafea1e
spirozh <jason.songhurst@gmail.com>
2023-07-20 00:17:38 +0200

    more testcases

--- http/routes_test.go
+++ http/routes_test.go
@@ -7,36 +7,65 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
+	"github.com/spirozh/timr/html"
 	"github.com/spirozh/timr/test"
+	"github.com/spirozh/timr/timer"
 )
 
 type selmaTestCase struct {
 	method       string
+	path         string
 	requestBody  string
 	status       int
 	responseBody string
 }
 
-func Test_Selma(t *testing.T) {
+func Test_Routes(t *testing.T) {
+
+	indexR, err := html.FS.Open("index.html")
+	test.Equal(t, nil, err)
+	index, _ := io.ReadAll(indexR)
 
 	testCases := []selmaTestCase{
-		{http.MethodGet, "", http.StatusOK, "hello selma!!\n"},
-		{http.MethodHead, "", http.StatusOK, ""},
-		{http.MethodDelete, "", http.StatusMethodNotAllowed, ""},
-		{http.MethodPost, "garbage", http.StatusMethodNotAllowed, ""},
-		{http.MethodPut, "garbage", http.StatusMethodNotAllowed, ""},
+		// filesystem
+		{http.MethodGet, "/", "", http.StatusOK, string(index)},
+		{http.MethodPost, "/", "ignored", http.StatusOK, string(index)},  // All methods act like GET
+		{http.MethodTrace, "/", "ignored", http.StatusOK, string(index)}, // All methods act like GET
+		{http.MethodHead, "/", "", http.StatusOK, ""},                    // Except, of course, HEAD
+
+		{http.MethodGet, "/js", "", http.StatusMovedPermanently, ""},
+		{http.MethodGet, "/js/", "", http.StatusOK, "<pre>\n<a href=\"timr.js\">timr.js</a>\n</pre>\n"},
+
+		{http.MethodGet, "/zz", "", http.StatusNotFound, "404 page not found\n"},
+
+		// selma
+		{http.MethodGet, "/selma/", "", http.StatusOK, "hello selma!!\n"},
+		{http.MethodHead, "/selma/", "", http.StatusOK, ""},
+		{http.MethodDelete, "/selma/", "", http.StatusMethodNotAllowed, ""},
+		{http.MethodPost, "/selma/", "ignored", http.StatusMethodNotAllowed, ""},
+		{http.MethodPut, "/selma/", "ignored", http.StatusMethodNotAllowed, ""},
+		{http.MethodPatch, "/selma/", "ignored", http.StatusMethodNotAllowed, ""},
+
+		// redirects to selma
+		{http.MethodGet, "/selma", "", http.StatusMovedPermanently, "<a href=\"/selma/\">Moved Permanently</a>.\n\n"},
+		{http.MethodPost, "/selma", "ignored", http.StatusMovedPermanently, ""},
 	}
 
-	for _, testCase := range testCases {
+	ts := timer.TimerService(func() time.Time { return time.Time{} })
+	routes := routes(ts)
+
+	for i, testCase := range testCases {
+		fmt.Printf("testcase: %d) %#v\n", i, testCase)
 		var requestBody io.Reader
 		if testCase.requestBody != "" {
 			requestBody = strings.NewReader(testCase.requestBody)
 		}
-		r := httptest.NewRequest(testCase.method, "/selma", requestBody)
+		r := httptest.NewRequest(testCase.method, testCase.path, requestBody)
 		w := httptest.NewRecorder()
 
-		selma(w, r)
+		routes.ServeHTTP(w, r)
 		res := w.Result()
 
 		test.Equal(t, testCase.status, res.StatusCode)
@@ -48,8 +77,5 @@ func Test_Selma(t *testing.T) {
 		data, err := io.ReadAll(res.Body)
 		test.Equal(t, nil, err)
 		test.Equal(t, testCase.responseBody, string(data))
-
-		fmt.Printf("%#v\n", res.Header)
 	}
-
 }
