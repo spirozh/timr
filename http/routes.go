58b88089c4a730f926424e0c532536ac24ad0396
spirozh <jason.songhurst@gmail.com>
2023-07-20 00:13:43 +0200

    inline selma handlerFunc

--- http/routes.go
+++ http/routes.go
@@ -12,7 +12,18 @@ func routes(ts timr.TimerService) http.Handler {
 	m := http.NewServeMux()
 
 	// register selma
-	m.HandleFunc("/selma/", selma)
+	m.HandleFunc("/selma/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			io.WriteString(w, "hello selma!!\n")
+			return
+		case http.MethodHead:
+			return
+		default:
+			w.Header().Add("Allow", "GET, HEAD")
+			w.WriteHeader(405)
+		}
+	})
 
 	// register file server
 	m.Handle("/", http.FileServer(http.FS(html.FS)))
@@ -25,17 +36,4 @@ func routes(ts timr.TimerService) http.Handler {
 	apiMux.HandleFunc("/api/timer/sse", SSE(ts))
 
 	return m
-}
-
-func selma(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		io.WriteString(w, "hello selma!!\n")
-		return
-	case http.MethodHead:
-		return
-	default:
-		w.Header().Add("Allow", "GET, HEAD")
-		w.WriteHeader(405)
-	}
 }
