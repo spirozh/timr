797cd27627ab21e3651a23396df92c3c08f18835
spirozh <jason.songhurst@gmail.com>
2025-07-06 19:47:39 +0200

    adjust and simplify routes_test

--- src/internal/routes_test.go
+++ src/internal/routes_test.go
@@ -7,23 +7,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestStaticRouteVsIndex(t *testing.T) {
+func TestRoutes(t *testing.T) {
 	mux := http.NewServeMux()
 	a := NewApp()
 	a.AddRoutes(mux)
 
 	testCases := map[string]struct {
-		method string
-		path   string
-		pat    string
+		path string
+		pat  string
 	}{
-		"index":  {"GET", "/", "GET /"},
-		"kill":   {"GET", "/kill", "GET /kill"},
-		"styles": {"GET", "/static/styles.css", "GET /static/"},
+		"index":  {"/", "GET /{$}"},
+		"404":    {"/foo", ""},
+		"kill":   {"/kill", "GET /kill"},
+		"styles": {"/static/styles.css", "GET /static/"},
 	}
 	for name, testCase := range testCases {
 		t.Run(name, func(t *testing.T) {
-			r, err := http.NewRequest(testCase.method, "http://localhost"+testCase.path, nil)
+			r, err := http.NewRequest("GET", "http://localhost"+testCase.path, nil)
 			assert.NoError(t, err)
 			_, pat := mux.Handler(r)
 			assert.Equal(t, testCase.pat, pat)
