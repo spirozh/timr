8c1a131991d9770cb2c26bae1963fc4b8c1563c3
spirozh <jason.songhurst@gmail.com>
2025-07-06 20:18:23 +0200

    add static handler

--- src/internal/view/static.go
+++ src/internal/view/static.go
@@ -1,6 +1,13 @@
 package view
 
-import "embed"
+import (
+	"embed"
+	"net/http"
+)
 
 //go:embed static/*
 var static embed.FS
+
+func StaticHandler() http.Handler {
+	return http.FileServer(http.FS(static))
+}
